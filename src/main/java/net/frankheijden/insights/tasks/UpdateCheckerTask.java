92baa113256ad3ad5db3e1baca59fa05f546d74c
Frank van der Heijden <frank.boekanier@gmail.com>
2020-02-16 14:49:16 +0100

    v2.10.0 - Implement our own plugin updater.

--- src/main/java/net/frankheijden/insights/tasks/UpdateCheckerTask.java
+++ src/main/java/net/frankheijden/insights/tasks/UpdateCheckerTask.java
@@ -1,64 +1,139 @@
 package net.frankheijden.insights.tasks;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.frankheijden.insights.Insights;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.inventivetalent.update.spiget.SpigetUpdate;
-import org.inventivetalent.update.spiget.UpdateCallback;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
 
 public class UpdateCheckerTask implements Runnable {
     private Insights plugin;
     private Player player;
+    private String currentVersion;
+    private boolean downloading;
+    private boolean downloaded;
+    private boolean error;
+
+    private static String GITHUB_INSIGHTS_LINK = "https://api.github.com/repos/FrankHeijden/Insights/releases/latest";
 
     public UpdateCheckerTask(Insights plugin, Player player) {
         this.plugin = plugin;
         this.player = player;
+        this.currentVersion = plugin.getDescription().getVersion();
+        this.downloading = false;
+        this.downloaded = false;
+        this.error = false;
     }
 
     @Override
     public void run() {
-        SpigetUpdate spigetUpdate = new SpigetUpdate(plugin, 56489);
-        spigetUpdate.checkForUpdate(new UpdateCallback() {
-            @Override
-            public void updateAvailable(String versionAvailable, String downloadUrl, boolean hasDirectDownload) {
-                String versionInstalled = plugin.getDescription().getVersion();
-                if (plugin.getConfiguration().GENERAL_UPDATES_DOWNLOAD) {
-                    if (plugin.getVersionQueued() != null) {
-                        if (plugin.getVersionQueued().equalsIgnoreCase(versionAvailable)) {
-                            plugin.getUtils().sendMessage(player, "messages.update.downloaded", "%old%", versionInstalled, "%new%", versionAvailable);
-                            return;
-                        }
-                    }
+        JsonObject jsonObject;
+        try {
+            jsonObject = readJsonFromURL(GITHUB_INSIGHTS_LINK).getAsJsonObject();
+        } catch (IOException ex) {
+            throw new RuntimeException("Error downloading a new version of Insights", ex);
+        }
+        String githubVersion = jsonObject.getAsJsonPrimitive("tag_name").getAsString();
+        githubVersion = githubVersion.replace("v", "");
+        String body = jsonObject.getAsJsonPrimitive("body").getAsString();
 
-                    if (plugin.isDownloading()) {
-                        plugin.addNotifyPlayer(player);
-                        return;
-                    }
-
-                    String path = null;
-                    if (hasDirectDownload) {
-                        plugin.setDownloading(true);
-                        if (spigetUpdate.downloadUpdate()) {
-                            path = "messages.update.downloaded";
-                            plugin.setVersionQueued(versionAvailable);
-                        }
-                    }
-
-                    if (path == null) path = "messages.update.download_failed";
-
-                    final String path_ = path;
-                    plugin.getUtils().sendMessage(player, path, "%old%", versionInstalled, "%new%", versionAvailable);
-                    plugin.getNotifyPlayers().forEach((p) -> plugin.getUtils().sendMessage(player, path_, "%old%", versionInstalled, "%new%", versionAvailable));
-                    plugin.clearNotifyPlayers();
-                    plugin.setDownloading(false);
-                } else {
-                    plugin.getUtils().sendMessage(player, "messages.update.available", "%old%", versionInstalled, "%new%", versionAvailable);
+        JsonArray assets = jsonObject.getAsJsonArray("assets");
+        String downloadLink = null;
+        if (assets != null && assets.size() > 0) {
+            downloadLink = assets.get(0).getAsJsonObject().getAsJsonPrimitive("browser_download_url").getAsString();
+        }
+        if (isNewVersion(githubVersion)) {
+            if (plugin.getConfiguration().GENERAL_UPDATES_DOWNLOAD) {
+                plugin.getUtils().sendMessage(player, "messages.update.downloading", "%old%", currentVersion, "%new%", githubVersion, "%info%", body);
+                if (downloading || (downloaded && currentVersion.equals(githubVersion) && !error)) {
+                    return;
                 }
-            }
+                downloaded = false;
 
-            @Override
-            public void upToDate() {
-                //
+                if (downloadLink == null) {
+                    status(githubVersion, true);
+                    return;
+                }
+                downloading = true;
+
+                try {
+                    download(downloadLink, getPluginFile());
+                } catch (IOException ex) {
+                    status(githubVersion, true);
+                    throw new RuntimeException("Error downloading a new version of Insights", ex);
+                }
+
+                status(githubVersion, false);
+                downloading = false;
+                downloaded = true;
+            } else {
+                plugin.getUtils().sendMessage(player, "messages.update.available", "%old%", currentVersion, "%new%", githubVersion, "%info%", body);
+            }
+        }
+    }
+
+    private void status(String githubVersion, boolean isError) {
+        final String path = "messages.update.download_" + (isError ? "failed" : "success");
+        Bukkit.getOnlinePlayers().forEach((p) -> {
+            if (p.hasPermission("insights.notification.update")) {
+                plugin.getUtils().sendMessage(player, path, "%new%", githubVersion);
             }
         });
+    }
+
+    private File getPluginFile() {
+        try {
+            Method method = JavaPlugin.class.getDeclaredMethod("getFile");
+            method.setAccessible(true);
+            return (File) method.invoke(this.plugin);
+        } catch (ReflectiveOperationException ex) {
+            throw new RuntimeException("Error retrieving current plugin file", ex);
+        }
+    }
+
+    private void download(String urlString, File target) throws IOException {
+        URL url = new URL(urlString);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(target);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    }
+
+    private boolean isNewVersion(String version) {
+        String[] currentVersion = this.currentVersion.split("\\.");
+        String[] newVersion = version.split("\\.");
+
+        int i = 0;
+        while (i < currentVersion.length && i < newVersion.length) {
+            if (Integer.parseInt(newVersion[i]) > Integer.parseInt(currentVersion[i])) return true;
+            i++;
+        }
+        return false;
+    }
+
+    private String readAll(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = reader.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    private JsonElement readJsonFromURL(String url) throws IOException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(reader);
+            return new JsonParser().parse(jsonText);
+        }
     }
 }
