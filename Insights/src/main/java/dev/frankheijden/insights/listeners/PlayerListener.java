431134793aba61622622a4a77c6b35ef9f15eca0
Frank van der Heijden <frank.boekanier@gmail.com>
2021-02-14 15:34:33 +0100

    Implement an update checker

--- Insights/src/main/java/dev/frankheijden/insights/listeners/PlayerListener.java
+++ Insights/src/main/java/dev/frankheijden/insights/listeners/PlayerListener.java
@@ -2,6 +2,8 @@ package dev.frankheijden.insights.listeners;
 
 import dev.frankheijden.insights.api.InsightsPlugin;
 import dev.frankheijden.insights.api.listeners.InsightsListener;
+import dev.frankheijden.insights.api.tasks.UpdateCheckerTask;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,9 +14,17 @@ public class PlayerListener extends InsightsListener {
         super(plugin);
     }
 
+    /**
+     * Handles the PlayerJoinEvent, updating the concurrent PlayerList and checking for updates.
+     */
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        plugin.getPlayerList().addPlayer(event.getPlayer());
+        Player player = event.getPlayer();
+        plugin.getPlayerList().addPlayer(player);
+
+        if (player.hasPermission("insights.update")) {
+            UpdateCheckerTask.check(player);
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
