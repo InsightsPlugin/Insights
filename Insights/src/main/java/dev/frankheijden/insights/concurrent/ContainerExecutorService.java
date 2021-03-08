1959070c52a50ce327ff4f69e3c30359446fbfa3
Frank van der Heijden <F.N.M.vanderHeijden@student.tudelft.nl>
2021-03-08 22:36:09 +0100

    Name our worker threads + uncaught exception logging

--- Insights/src/main/java/dev/frankheijden/insights/concurrent/ContainerExecutorService.java
+++ Insights/src/main/java/dev/frankheijden/insights/concurrent/ContainerExecutorService.java
@@ -1,11 +1,14 @@
 package dev.frankheijden.insights.concurrent;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import dev.frankheijden.insights.api.InsightsPlugin;
 import dev.frankheijden.insights.api.concurrent.ContainerExecutor;
 import dev.frankheijden.insights.api.concurrent.containers.RunnableContainer;
 import dev.frankheijden.insights.api.concurrent.containers.SupplierContainer;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
 
 public class ContainerExecutorService implements ContainerExecutor {
 
@@ -15,8 +18,20 @@ public class ContainerExecutorService implements ContainerExecutor {
         this.executorService = executorService;
     }
 
+    /**
+     * Constructs a new containerexecutor with given amount of worker threads.
+     */
     public static ContainerExecutor newExecutor(int nThreads) {
-        return new ContainerExecutorService(Executors.newFixedThreadPool(nThreads));
+        return new ContainerExecutorService(Executors.newFixedThreadPool(
+                nThreads,
+                new ThreadFactoryBuilder()
+                        .setNameFormat("Insights-worker-%d")
+                        .setUncaughtExceptionHandler((t, e) -> InsightsPlugin.getInstance().getLogger().log(
+                                Level.SEVERE,
+                                String.format("[%s] Error occurred on worker thread:", t.getName()),
+                                e
+                        )).build()
+        ));
     }
 
     @Override
