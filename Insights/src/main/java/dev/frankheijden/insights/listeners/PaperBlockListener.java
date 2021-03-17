9f714aacf2fb448a210d3ae43096f9812112248e
Frank van der Heijden <frank.boekanier@gmail.com>
2021-03-17 20:13:47 +0100

    Add indirect block listener for paper This allows the updates from cactus being broken from bottom to top etc to come through.

---
+++ Insights/src/main/java/dev/frankheijden/insights/listeners/PaperBlockListener.java
@@ -0,0 +1,23 @@
+package dev.frankheijden.insights.listeners;
+
+import com.destroystokyo.paper.event.block.BlockDestroyEvent;
+import dev.frankheijden.insights.api.InsightsPlugin;
+import dev.frankheijden.insights.api.annotations.AllowDisabling;
+import dev.frankheijden.insights.api.listeners.InsightsListener;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
+public class PaperBlockListener extends InsightsListener {
+
+    public PaperBlockListener(InsightsPlugin plugin) {
+        super(plugin);
+    }
+
+    @AllowDisabling
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockDestroy(BlockDestroyEvent event) {
+        Block block = event.getBlock();
+        handleModification(block.getLocation(), block.getType(), event.getNewState().getMaterial(), 1);
+    }
+}
