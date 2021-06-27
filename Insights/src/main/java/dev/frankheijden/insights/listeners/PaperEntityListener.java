0022474de17adb2c3e92ceafd17bc219beae5a54
Frank van der Heijden <frank.boekanier@gmail.com>
2021-06-28 01:24:52 +0200

    Handle TNT Prime event on paper servers

--- Insights/src/main/java/dev/frankheijden/insights/listeners/PaperEntityListener.java
+++ Insights/src/main/java/dev/frankheijden/insights/listeners/PaperEntityListener.java
@@ -1,6 +1,9 @@
 package dev.frankheijden.insights.listeners;
 
+import com.destroystokyo.paper.event.block.TNTPrimeEvent;
 import dev.frankheijden.insights.api.InsightsPlugin;
+import dev.frankheijden.insights.api.objects.wrappers.ScanObject;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
@@ -16,5 +19,22 @@ public class PaperEntityListener extends EntityListener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityRemove(com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent event) {
         handleEntityRemoveFromWorld(event.getEntity());
+    }
+
+    /**
+     * Handles the TNTPrimeEvent for ignited TNT blocks.
+     */
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onTNTPrime(TNTPrimeEvent event) {
+        var primerEntity = event.getPrimerEntity();
+        var block = event.getBlock();
+
+        if (primerEntity instanceof Player) {
+            var player = (Player) primerEntity;
+
+            handleRemoval(player, block.getLocation(), ScanObject.of(block.getType()), 1, false);
+        } else {
+            handleModification(block, -1);
+        }
     }
 }
