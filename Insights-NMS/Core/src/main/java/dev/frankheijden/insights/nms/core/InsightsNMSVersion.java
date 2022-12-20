6d0d38d3dce4ca100c0bf2de1c0137f7f2a5785f
mateusneresrb <mateusneresrb@gmail.com>
2022-12-19 22:40:49 -0300

    Add support to 1.19.3 version

--- Insights-NMS/Core/src/main/java/dev/frankheijden/insights/nms/core/InsightsNMSVersion.java
+++ Insights-NMS/Core/src/main/java/dev/frankheijden/insights/nms/core/InsightsNMSVersion.java
@@ -3,4 +3,5 @@ package dev.frankheijden.insights.nms.core;
 public enum InsightsNMSVersion {
     v1_19_1_R1,
     v1_19_2_R1,
+    v1_19_3_R2,
 }
---
+++ Insights-NMS/v1_19_3_R2/build.gradle.kts
@@ -0,0 +1,3 @@
+dependencies {
+    paperDevBundle("1.19.3-R0.1-SNAPSHOT")
+}
