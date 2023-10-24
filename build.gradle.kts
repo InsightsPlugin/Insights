9b6cf75d60b01920791439300bde00c0f5b652be
Frank van der Heijden <frank.boekanier@gmail.com>
2023-10-24 17:35:47 +0200

    v6.16.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val name = "Insights"
 group = "dev.frankheijden.insights"
 val dependencyDir = "$group.dependencies"
-version = "6.16.1"
+version = "6.16.2-SNAPSHOT"
 
 subprojects {
     apply(plugin = "java")
