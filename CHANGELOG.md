5544ee4abadfd7166c31f6c6f5c3418b72223180
Frank van der Heijden <frank.boekanier@gmail.com>
2024-01-12 15:23:16 +0100

    Back to snapshots

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,1 @@
 # Changelog
-- Automatic releases to modrinth via GitHub Actions
-- No further changes are made comapared to v6.17.1!
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val name = "Insights"
 group = "dev.frankheijden.insights"
 val dependencyDir = "$group.dependencies"
-version = "6.17.2"
+version = "6.17.3-SNAPSHOT"
 
 subprojects {
     apply(plugin = "java")
