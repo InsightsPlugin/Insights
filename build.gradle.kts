70bf352d70fd5541a929081b6097a79a6328e640
funniray <funniray10@gmail.com>
2025-01-14 21:09:23 +0000

    1.21.4 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ subprojects {
         maven("https://repo.codemc.io/repository/maven-public")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://libraries.minecraft.net")
     }
 
